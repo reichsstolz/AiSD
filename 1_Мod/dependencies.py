97de7b031c02b5fe4894567de99970b1e7564842
reichsstolz <styrkoffpost@gmail.com>
2022-10-04 00:13:01 +0300

    solutions to 1_M

--- 1_Мod/dependencies.py
+++ 1_Мod/dependencies.py
@@ -1,4 +1,33 @@
+import sys
 import sys as sus
+
+
+class Path:
+    def __init__(self, start):
+        self.path = [start]
+
+    def in_path(self, node):
+        return node in self.path
+
+    def update(self):
+        if self.is_end():
+            return {self}
+        return set(self.clone(node) for node in self.path[0].parent if self.clone(node))
+
+    def clone(self, node):
+        new = Path(None)
+        new.path = self.path.copy()
+        if not new.in_path(node):
+            new.path.insert(0, node)
+            return new
+
+    def is_end(self):
+        if self.path[0].parent:
+            return False
+        return True
+
+    def __str__(self):
+       return " ".join([x.name for x in self.path[1:]])
 
 
 class Dependency:
@@ -12,19 +41,13 @@ class Dependency:
         self.parent.add(parent)
 
     def get_path(self):
-        paths = [self.name]
-        current = {self}
-        while [x for x in current if x.parent]:
-            for i in range(len(paths)):
-                for lib in current:
-                    if paths[i].split()[0] == lib.name and lib.parent:
-                        for parent in lib.parent:
-                            paths.append(f"{parent.name} " + paths[i])
-                        paths.remove(paths[i])
+        paths = {Path(self)}
+        while False in [x.is_end() for x in paths]:
             buffer = set()
-            for s in map(lambda x: x.parent, current):
-                buffer.update(s)
-            current = buffer
+            for p in map(lambda path: path.update(), paths):
+                if p:
+                    buffer.update(p)
+            paths = buffer
         return paths
 
 
@@ -53,28 +76,30 @@ class Graph:
         for node in self.nodes:
             if node.name in self.vulnerable:
                 for path in node.get_path():
-                    if self.head.name in path:
-                        print(path)
+                    if path.in_path(self.head):
+                        print(str(path))
 
 
 def main():
-    test = open("test.txt", "r")
-    vulnerable = set(test.readline().split())
-    line = test.readline().split()
+    #test = open("test.txt", "r")
+    vulnerable = set(input().split())
+    line = input().split()
     graph = Graph(Dependency('project'), vulnerable)
 
     for i in line:
         graph.get_child(i, graph.head)
 
-    for line in test:
+    for line in sys.stdin:
         line = line.replace('\n', '').split()
+        if not line:
+            continue
         parent = graph.get_parent(line[0])
 
         for name in line[1:]:
             graph.get_child(name, parent)
 
     graph.print_paths()
-    test.close()                
+    #test.close()
 
 
 if __name__ == '__main__':
