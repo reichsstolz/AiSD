db3df591853ee5a4ebd3f57d4048a6917b302ee0
reichsstolz <styrkoffpost@gmail.com>
2022-10-03 10:41:40 +0300

    renaming && fixes

--- dependencies.py
+++ dependencies.py
@@ -16,7 +16,7 @@ class Dependency:
     def get_path(self):
         paths = [self.name]
         current = {self}
-        while [x for x in current if not x.parent is None]:
+        while [x for x in current if x.parent]:
             for i in range(len(paths)):
                 for lib in current:
                     if paths[i].split()[0] == lib.name and lib.parent:
@@ -30,7 +30,7 @@ class Dependency:
         return paths
 
 
-def find_child(name, parent):
+def get_child(name, parent):
     for node in nodes:
         if node.name == name:
             node.add_parent(parent)
@@ -38,12 +38,11 @@ def find_child(name, parent):
     return Dependency(name, parent)
 
 
-def extract_nodes(name):
-    found = []
+def extract_node(name):
     for node in nodes:
         if node.name == name:
-            found.append(node)
-    return found
+            return node
+    return
 
 
 if __name__ == '__main__':
@@ -55,19 +54,18 @@ if __name__ == '__main__':
 
     for line in test:
         line = line.replace('\n', '').split()
-        extracted = extract_nodes(line[0])
+        extracted = extract_node(line[0])
 
         if not extracted:
             lost_lib = Dependency(line[0])
             nodes.add(lost_lib)
-            nodes.update(find_child(name, lost_lib) for name in line[1:])
+            nodes.update(get_child(name, lost_lib) for name in line[1:])
             continue
 
-        for item in extracted:
-            nodes.update(find_child(name, item) for name in line[1:])
-
-    nodes = set(x for x in nodes if x.parent)
+        nodes.update(get_child(name, extracted) for name in line[1:])
 
     for i in nodes:
         if i.name in vulnerable:
-            print(i.get_path())
+            for s in i.get_path():
+                if project.name in s:
+                    print(s)
